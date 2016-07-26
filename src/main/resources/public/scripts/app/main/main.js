262e3dfae2b8dbd5c875fc85f07dde5c9916f446
Nemeros <Nemeros>
2016-07-26 10:33:48 +0200

    eee

--- src/main/resources/public/scripts/app/main/main.js
+++ src/main/resources/public/scripts/app/main/main.js
@@ -13,22 +13,28 @@
 		
 		vm.lyon = {lat: 45.77699455, lng: 4.82488845, zoom: 10};
 		
-		$http.get("static/geo.json").then(
-				function successCallBack(response){
-					vm.geojson = {
-							data: response.data,
-							style: {
-								fillColor: 'green',
-								weight: 2,
-								opacity: 1,
-								color: 'red',
-								fillOpacity: 0.7
-							}
-					};
-					$log.info(angular.toJson(response))
-				},
-				function errorCallBack(response){
-					$log.error(angular.toJson(response));
-				});
+		vm.refreshData = refreshData;
+		
+		
+		function refreshData(){
+		
+			$http.get("api/traffic").then(
+					function successCallBack(response){
+						vm.geojson = {
+								data: response.data,
+								style: {
+									fillColor: 'green',
+									weight: 2,
+									opacity: 1,
+									color: 'red',
+									fillOpacity: 0.7
+								}
+						};
+						$log.info(angular.toJson(response))
+					},
+					function errorCallBack(response){
+						$log.error(angular.toJson(response));
+					});
+		};
 	}
 })();
