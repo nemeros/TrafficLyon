4fdb9c0a808403fdb04a18c45d6e3d7cd9bad33a
Nemeros <Nemeros>
2016-07-26 15:56:56 +0200

    ee

--- src/main/java/com/service/GrandLyonService.java
+++ src/main/java/com/service/GrandLyonService.java
@@ -1,9 +1,12 @@
 package com.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +15,8 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class GrandLyonService {
+	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@Value("${TRAFFIC_URL}")
 	private String url ;
@@ -27,6 +32,10 @@ public class GrandLyonService {
 		
 		HttpEntity<String> entity = new HttpEntity<String>(headers);
 		
-		return rt.exchange(url, HttpMethod.GET, entity, String.class);
+		ResponseEntity<String> rep = rt.exchange(url, HttpMethod.GET, entity, String.class);
+
+		log.info("Response : " + rep.getBody());
+		
+		return new ResponseEntity<String>(rep.getBody(), HttpStatus.OK);
 	}
 }
